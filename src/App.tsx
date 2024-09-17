29e94f6a244cb1a906c0b71ded65ebb7b776b56a
Jakob-Sholler <jakob.sholler@maine.edu>
2024-09-17 15:22:58 -0400

    Completed chapter 2 problems, adding a button, red background, red squares, image with alt text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 import React from "react";
 import "./App.css";
-
+import { Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                <h1>
+                    UM COS420 with React Hooks and TypeScript Converted into H1
+                    header
+                </h1>
+
+                <img
+                    src="https://static.wikia.nocookie.net/yakuza/images/4/4f/Chikenball.png/revision/latest?cb=20190428133133"
+                    alt="A picture of the Manager Chicken Nugget from the hit series Yakuza AKA Like a Dragon"
+                />
+
+                <ul>
+                    <li>Butter</li>
+                    <li>Milk</li>
+                    <li>Bills</li>
+                </ul>
+
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "500px",
+                                    height: "500px",
+                                    backgroundColor: "red",
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
@@ -14,5 +50,4 @@ function App(): React.JSX.Element {
         </div>
     );
 }
-
 export default App;
