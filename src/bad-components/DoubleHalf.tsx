4a2456e3ea93b53aec7c6a74048091fc3afb1a65
Jakob-Sholler <jakob.sholler@maine.edu>
2024-11-09 14:54:06 -0500

    Finished Double Half

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,40 +1,31 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
-
-function Doubler(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
-
+/*Removed the halfer and double function and put into one function.*/
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <div style={{ display: "flex", gap: "10px" }}>
+                <Button
+                    onClick={() => {
+                        setDhValue(2 * dhValue);
+                    }}
+                >
+                    Double
+                </Button>
+                <Button
+                    onClick={() => {
+                        setDhValue(0.5 * dhValue);
+                    }}
+                >
+                    Halve
+                </Button>
+            </div>
         </div>
     );
 }
